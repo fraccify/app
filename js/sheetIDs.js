b4db918a2213b28ac7a1a59ef4a085f6b4ab7c31
fraccify <168594714+fraccify@users.noreply.github.com>
2024-09-06 14:17:19 -0600

    Add files via upload

---
+++ js/sheetIDs.js
@@ -0,0 +1,37 @@
+export const sheetIDs = {
+    "Privada San Sebastian - Andalucia": {
+        sheetID: "20758fb4-773d-43c9-995b-dcc92cc09071",
+        sheetIDprivada: "",
+        sheetIDubicacion: "https://maps.app.goo.gl/MbB9UXEhoodqaW11A",
+        sheetIDCalender: "https://calendar.google.com/calendar/embed?src=37177ba39d333f7ac9edfc4ae9571b75e2562da9e2887771191bab6db82b5aee%40group.calendar.google.com&ctz=America%2FMexico_City",
+        banco: "SANTANDER",
+        titular: "COLONOS SAN SEBASTIAN, A.C.",
+        cuenta: "65509378739",
+        clabe: "014308655093787392",
+        monto: "$550.00 M.N.",
+        preferenceID: "1824470973-45e4405e-20d0-4500-8a97-47ce5c198d65",
+        calles: [
+            { nombre: "ALBA", codigo: "IkFMQk" },
+            { nombre: "CABALLEROS", codigo: "IkNBQkFMTEVST" },
+            { nombre: "ESMERALDA", codigo: "IkVTTUVSQUxEQ" },
+            { nombre: "EROS", codigo: "IkVST1" },
+            { nombre: "HIERRO", codigo: "IkhJRVJSTy" },
+            { nombre: "IBIZA", codigo: "IklCSVpBI" },
+            { nombre: "MAGDALENA", codigo: "Ik1BR0RBTEVOQ" }
+        ]    
+    },
+    "Guadalajara Country Club": {
+        sheetID: "20758fb4-773d-43c9-995b-dcc92cc09071",
+        sheetIDprivada: "-GCL",
+        // O cualquier otra cadena si es necesario
+    },
+    "Jardines de Tizayuca I": {
+        sheetID: "186ffa7a-fa1e-4afe-8b13-84496e34d390",
+        sheetIDprivada: "-JDTI", // Deja vacío o usa false según la necesidad
+        calles: ["Almendros", "Bugambilias", "Laureles", "Magnolias","Tulipanes","Violetas"] // Agregar las calles aquí
+
+    }
+};
+
+// Export the sheetIDs object for use in other files
+export default sheetIDs;
