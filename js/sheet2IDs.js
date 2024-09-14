2d3fbefc0cfb2a4dd1901831824cb6f89221cf0c
fraccify <168594714+fraccify@users.noreply.github.com>
2024-09-14 13:00:04 -0600

    Add files via upload

---
+++ js/sheet2IDs.js
@@ -0,0 +1,83 @@
+export const sheetIDs = {
+    "Privada San Sebastian - Andalucia": {
+        sheetID: "20758fb4-773d-43c9-995b-dcc92cc09071",
+        sheetIDprivada: "",
+        sheetIDubicacion: "https://maps.app.goo.gl/MbB9UXEhoodqaW11A",
+        sheetIDCalender: "https://calendar.google.com/calendar/embed?src=37177ba39d333f7ac9edfc4ae9571b75e2562da9e2887771191bab6db82b5aee%40group.calendar.google.com&ctz=America%2FMexico_City",
+        banco: "SANTANDER",
+        titular: "COLONOS SAN SEBASTIAN, A.C.",
+        cuenta: "65509378739",
+        clabe: "014308655093787392",
+        monto: "$550.00 M.N.",
+        preferenceID: "1824470973-45e4405e-20d0-4500-8a97-47ce5c198d65",
+        casetaseguridad: "Ok",
+        segurichat: "527713598364",
+        adminIA: "Si",
+        calles: [
+            { nombre: "ALBA", codigo: "IkFMQk" },
+            { nombre: "CABALLEROS", codigo: "IkNBQkFMTEVST" },
+            { nombre: "ESMERALDA", codigo: "IkVTTUVSQUxEQ" },
+            { nombre: "EROS", codigo: "IkVST1" },
+            { nombre: "HIERRO", codigo: "IkhJRVJSTy" },
+            { nombre: "IBIZA", codigo: "IklCSVpBI" },
+            { nombre: "MAGDALENA", codigo: "Ik1BR0RBTEVOQ" },
+            { nombre: "Seguridad", codigo: "IlNlZ3VyaWRhZC" },
+
+        ]    
+    },
+    "Guadalajara Country Club": {
+        sheetID: "20758fb4-773d-43c9-995b-dcc92cc09071",
+        sheetIDprivada: "-GCL",
+        sheetIDubicacion: "https://maps.app.goo.gl/AcT4bmdn3JoZqSfRA",
+        sheetIDCalender: "https://calendar.google.com/calendar/embed?src=37177ba39d333f7ac9edfc4ae9571b75e2562da9e2887771191bab6db82b5aee%40group.calendar.google.com&ctz=America%2FMexico_City",
+        banco: "BBVA",
+        titular: "Guadalajara Country Club",
+        cuenta: "12345678",
+        clabe: "12345678912345",
+        monto: "$1,550.00 M.N.",
+        preferenceID: "1824470973-45e4405e-20d0-4500-8a97-47ce5c198d65",
+        casetaseguridad: "Ok",
+        segurichat: "525582227469",
+        adminIA: "Si",
+        calles: [
+            { nombre: "ALBA", codigo: "IkFMQk" },
+            { nombre: "CABALLEROS", codigo: "IkNBQkFMTEVST" },
+            { nombre: "ESMERALDA", codigo: "IkVTTUVSQUxEQ" },
+            { nombre: "EROS", codigo: "IkVST1" },
+            { nombre: "HIERRO", codigo: "IkhJRVJSTy" },
+            { nombre: "IBIZA", codigo: "IklCSVpBI" },
+            { nombre: "MAGDALENA", codigo: "Ik1BR0RBTEVOQ" },
+            { nombre: "Seguridad", codigo: "IlNlZ3VyaWRhZC" },
+        ] 
+        // O cualquier otra cadena si es necesario
+    },
+    "Jardines de Tizayuca I": {
+        sheetID: "186ffa7a-fa1e-4afe-8b13-84496e34d390",
+        sheetIDprivada: "-JDTI", // Deja vacío o usa false según la necesidad
+        sheetIDubicacion: "https://maps.app.goo.gl/6ZetYc4mB1RQMJBD6",
+        sheetIDCalender: "",
+        banco: "HSBC",
+        titular: "Alvaro Edgar Delgado Guerrero",
+        cuenta: "6610540276 - Tarjeta: 4213166198975736",
+        clabe: "021180066105402763",
+        monto: "$250.00 M.N.",
+        preferenceID: "",
+        casetaseguridad: "Ok",
+        segurichat: "527714148096",
+        adminIA: "Si",
+        calles: [
+            { nombre: "Almendros", codigo: "IkFsbWV" },
+            { nombre: "Bugambilias", codigo: "IkJ1Z2F" },
+            { nombre: "Laureles", codigo: "IkxhdXJ" },
+            { nombre: "Magnolias", codigo: "Ik1hZ25" },
+            { nombre: "Tulipanes", codigo: "IlR1bGl" },
+            { nombre: "Violetas", codigo: "IlZpb2x" },
+            { nombre: "Seguridad", codigo: "IlNlZ3VyaWRhZC" },
+
+        ] // Agregar las calles aquí
+
+    }
+};
+
+// Export the sheetIDs object for use in other files
+export default sheetIDs;
